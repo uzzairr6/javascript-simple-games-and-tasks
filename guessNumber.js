ebb253e53d7030a2aa4ecb91c4a5ecabe4938991
uzzairr6 <110890928+uzzairr6@users.noreply.github.com>
2025-08-16 11:53:59 +0530

    Add files via upload

---
+++ guessNumber.js
@@ -0,0 +1,8 @@
+let n=30;
+//let x=prompt("guess no:")
+userGuess=prompt("guess the number")
+while(userGuess!=n)
+{
+    userGuess=prompt("wrong guess!try again")
+}
+console.log("congractulations")
