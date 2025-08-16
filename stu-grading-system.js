2a77ac84eecf6abbe05394575f8b85a4fe92fad4
uzzairr6 <110890928+uzzairr6@users.noreply.github.com>
2025-08-16 11:55:38 +0530

    Add files via upload

---
+++ stu-grading-system.js
@@ -0,0 +1,28 @@
+let grades=prompt("enter marks:")
+if (grades>=80 && grades<=100)
+{
+    console.log("A grade")
+}
+else if(grades>=70 && grades<=89)
+{
+    console.log("B grade")
+}
+else if(grades>=60 && grades<=69)
+{
+    console.log("C grade")
+}
+else if(grades>=50 && grades<=59)
+{
+    console.log("D grade")
+}
+else if(grades>=0 && grades<=49)
+{
+    console.log("fail")
+}
+else
+{
+    console.log("inavlid marks")
+}
+a=2
+b=3
+console.log(a**b)
